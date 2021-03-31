6c980a93a4c5ba611770a998e96d113de5fd6b47
Jared Leuz <jsleuz@liberty.edu>
2021-03-31 13:54:25 -0400

    Test

---
+++ Lab3B.cpp
@@ -0,0 +1,32 @@
+// Lab3B.cpp      Jared Leuz       CSIS 111
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> g1;
+
+    for (int i = 0; i <= 5; i++)
+    {
+        g1.push_back(i);
+    }
+    g1.insert(g1.begin(), 61);
+
+    cout << "Number in vector: ";
+    for (int i = 0; i < g1.size(); i++)
+    {
+        cout << g1[i] << " ";
+    }
+
+    cout << "\n\nNumber in reverse: ";
+    for (int i = g1.size() - 1; i >= 0; i--)
+    {
+        cout << g1[i] << " ";
+    }
+
+    cout << "\n\nEnd of Program\n\n";
+    system("pause");
+    return 0;
+}
